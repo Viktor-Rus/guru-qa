a7ef92dd29bfdc5a464e7d95d4bd0d9569e1035c
Viktor_Rus <vaurusov@mts.ru>
2022-07-28 11:55:34 +0300

    first commit

---
+++ src/main/java/PlayerVolleyball.java
@@ -0,0 +1,25 @@
+public class PlayerVolleyball {
+     int height;
+     String name;
+     String position;
+
+     public PlayerVolleyball(int height, String name, String position){
+            this.height = height;
+            this.name = name;
+            this.position = position;
+     }
+
+    public PlayerVolleyball(String name, String position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public PlayerVolleyball(){
+    }
+
+    void atack(){
+
+    }
+
+
+}
